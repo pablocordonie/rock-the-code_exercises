b4d0e6a279389ec31ef93dd23340c9e2f5f9a6a7
pablocordonie <pablogarcia.93.06@gmail.com>
2022-12-03 18:47:30 +0100

    Exercise 10 -> solvethese arrays with their corresponding methods

--- Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/10-array-metodos/index.js
+++ Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/10-array-metodos/index.js
@@ -1,38 +1,47 @@
 // 1. Asigna un array de lo que quieras a la variable "marks", después
 // asigna la longitud de "marks" a la variable "length"
-var marks;
-var length;
+var marks = [10, 4, 6, 3, 7];
+var length = marks.length;
 
 // 2. Dada la variable "colores", asigna la posición del array
 // colores necesaria a la variable "azul" para que su valor sea "azul"
-var colores = ["naranja", "rojo", "azul"];
-var azul;
+var colores = ['naranja', 'rojo', 'azul'];
+var azul = colores[2];
 
 // 3. Dada la variable "posiciones", añade en la primera posición
 // el string "primero"
-var posiciones = ["segundo", "tercero"];
+var posiciones = ['segundo', 'tercero'];
+
+posiciones.unshift('primero');
 
 // 4. Dada la variable "secuencia", elimínale la primera posición
 var secuencia = [14, 1, 2, 3, 4, 5];
 
+secuencia.shift();
+
 // 5. Dada la variable "puntosCardinales", complétala añadiendo en la última
 // posición el string "oeste"
-var puntosCardinales = ["norte", "sur", "este"];
+var puntosCardinales = ['norte', 'sur', 'este'];
+
+puntosCardinales.push('oeste');
 
 // 6. Dada la variable "frutas", elimínale la última posición
-var frutas = ["manzana", "naranja", "uva", "berenjena"];
+var frutas = ['manzana', 'naranja', 'uva', 'berenjena'];
+
+frutas.pop();
 
 // 7. Dada la variable "animales", asigna a la variable "animalesTerrestres" una
 // copia de los animales que sean terrestres con la funcion "slice"
-var animales = ["delfin", "tiburon", "elefante", "perro", "aguila", "cuervo"];
-var animalesTerrestres;
+var animales = ['delfin', 'tiburon', 'elefante', 'perro', 'aguila', 'cuervo'];
+var animalesTerrestres = animales.slice(2, 4);
 
 // --EXTRA-- Dada la variable "peliculas", asigna a la variable "peliculasCopiaSinReferencia" una copia nueva
 // de "peliculas" con diferente referencia y asigna a la variable "peliculasCopiaConReferencia" una copia
 // con la misma referencia
-var peliculas = ["el padrino", "star wars", "avatar"];
-var peliculasCopiaSinReferencia;
-var peliculasCopiaConReferencia;
+var peliculas = ['el padrino', 'star wars', 'avatar'];
+var peliculasCopiaSinReferencia = peliculas.slice();
+peliculas.push('gladiator');
+var peliculasCopiaConReferencia = peliculas.slice();
 
 // ///////////////////////////////////////////////////////////////
 // ⬇️ NO TOCAR ESTA PARTE O ROMPERÁS LOS TESTS ⬇️
