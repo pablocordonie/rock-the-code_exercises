9336aa92cbafc6fe6eb8bc736dc94cc37de6cff5
pablocordonie <pablogarcia.93.06@gmail.com>
2022-12-03 18:09:48 +0100

    Exercise 9 -> solve these basic functions

--- Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/09-funciones/index.js
+++ Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/09-funciones/index.js
@@ -1,19 +1,38 @@
 // 1. Declara una función llamada "getLongString" que devuelva un string conteniendo al menos 6 caracteres.
 // Ejemplo: getLongString() tiene que devolver un string de longitud igual o mayor a '123456'.
-function getLongString() {}
+function getLongString() {
+  const userString = '123456';
+  if (userString.length >= 6) {
+    return userString;
+  }
+}
+
+getLongString();
 
 // 2. Declara una función llamada "getInUpperCase" que devuelva el string pasado como argumento en MAYÚSCULAS.
 // Ejemplo: getInUpperCase('thepower') tiene que devolver THEPOWER.
-function getInUpperCase(text) {}
+function getInUpperCase(text) {
+  return text.toUpperCase();
+}
+
+getInUpperCase('thepower');
 
 // 3. Declara una función llamada "sum" que devuelva dos números pasados como argumentos (a, b) sumados.
 // Ejemplo: sum(2, 4) tiene que devolver 6.
-function sum(a, b) {}
+function sum(a, b) {
+  return a + b;
+}
+
+sum(2, 4);
 
 // 4. Declara una función llamada "sliceUntil" que devuelva un trozo de un string pasado como argumento (usando .slice sobre este),
 // que empiece en el índice 0 y acabe en el índice que tenga el valor del segundo argumento pasado a la función.
 // Ejemplo: sliceUntil('thepower school', 6) tiene que devolver 'thepow'
-function sliceUntil(text, limit) {}
+function sliceUntil(text, limit) {
+  return text.slice(0, limit);
+}
+
+sliceUntil('thepower school', 6);
 
 // ///////////////////////////////////////////////////////////////
 // ⬇️ NO TOCAR ESTA PARTE O ROMPERÁS LOS TESTS ⬇️
