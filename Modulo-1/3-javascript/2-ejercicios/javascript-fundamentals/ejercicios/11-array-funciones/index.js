d193e212d9e7ab45fb1e10fe53b37fc56b772b7d
pablocordonie <pablogarcia.93.06@gmail.com>
2022-12-08 18:09:26 +0100

    Exercise 11 -> solve these functions with array methods

--- Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/11-array-funciones/index.js
+++ Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/11-array-funciones/index.js
@@ -1,15 +1,44 @@
 // 1. Crea la función "getNewArray" que reciba un  array de frutas, y
 // un string con otra fruta nueva, y luego retorne otro array igual (pero con
 // otra referencia) pero habiéndole añadido en la primera posición la fruta nueva.
+let fruits = ['banana', 'naranja', 'manzana'];
+const newFruit = 'mango';
+
+const getNewArray = (arr, fruit) => {
+  const copyFruitsArray = arr.slice();
+  copyFruitsArray.unshift(fruit);
+  return copyFruitsArray;
+};
+
+getNewArray(fruits, newFruit);
 
 // 2. Crea la función "modifyOriginalArray" que reciba un array de números o strings,
 // le elimine la primera y la última posición y lo retorne (misma referencia)
+const modifyOriginalArray = (arr) => {
+  arr.shift();
+  arr.pop();
+  return arr;
+};
+
+modifyOriginalArray(fruits);
 
 // --EXTRA-- Dado el array "colors", crea la función "getColors" que reciba un número
 // como parámetro, cree una copia, y añada el color "black" en la última posición de
 // dicha copia si el número es par, o añada el color "white" en la primera posición
 // si es impar, y luego retorne dicho array
-const colors = ["blue", "red", "gray"];
+const colors = ['blue', 'red', 'gray'];
+
+const getColors = (number) => {
+  const copyColors = colors.slice();
+  if (number % 2 === 0) {
+    copyColors.push('black');
+  } else {
+    copyColors.unshift('white');
+  }
+  return copyColors;
+};
+
+getColors(3);
 
 // ///////////////////////////////////////////////////////////////
 // ⬇️ NO TOCAR ESTA PARTE O ROMPERÁS LOS TESTS ⬇️
