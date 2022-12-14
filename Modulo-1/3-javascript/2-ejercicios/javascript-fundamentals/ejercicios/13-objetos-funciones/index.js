baf1dae859f14145f439b96ce87c9047811afa0d
pablocordonie <pablogarcia.93.06@gmail.com>
2022-12-14 14:42:33 +0100

    Exercise 13 -> solve these functions with everything learned before this exercise

--- Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/13-objetos-funciones/index.js
+++ Modulo-1/3-javascript/2-ejercicios/javascript-fundamentals/ejercicios/13-objetos-funciones/index.js
@@ -1,23 +1,68 @@
 // 1. Dada la función "printPersons" que recibe un array de objetos llamado "persons",
 // y un string "property", completa la función para que recorra dicho array con un
 // bucle e invoque un console.log con la propiedad "property" de cada objeto
-const printPersons = (persons, property) => {
-  // Completa el código ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
-  // Completa el código ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+
+const persons = [
+  { name: 'Jon', surname: 'Snow' },
+  { name: 'Daenerys', surname: 'Targaryen' }
+];
+
+const printPersons = (arr, key) => {
+  for (let i = 0; i < arr.length; i++) {
+    const person = persons[i];
+    if (person[key] === 'Jon' || 'Snow' || 'Daenerys' || 'Targaryen') {
+      console.log(person[key]);
+    }
+  }
 };
+
+printPersons(persons, 'name');
+printPersons(persons, 'surname');
 
 // 2. Crea la función addProperty para que reciba un objeto, una propiedad y valor nuevos,
 // y un boolean. Si el boolean es true, que cree una copia de dicho objeto, añada la
 // nueva propiedad + valor y lo retorne. Si el boolean es false, que retorne directamente
 // el mismo objeto (misma referencia)
-const addProperty = () => { };
+
+const person = {
+  name: 'Pablo',
+  surname: 'Cordonié'
+};
+
+const addProperty = (object, key, value, boolean) => {
+  if (boolean) {
+    const copyTruePerson = Object.assign(object, object);
+    copyTruePerson[key] = value;
+    return copyTruePerson;
+  } else {
+    const copyFalsePerson = Object.assign(object, object);
+    return copyFalsePerson;
+  }
+};
+
+addProperty(person, 'oldSurname', 'García', true);
 
 // 3. Crea la función getPeopleWeight para que reciba un array con [clave, valor,...]
 // donde "clave" es el nombre de las personas y "valor" su peso en kg, y devuelva
 // un objeto con solo las personas (clave) y su peso (valor) que tengan un peso
 // par y por encima de 60kg.
-// Example of array => peopleArr = ["Juan", 72, "Maria", 58, "Martin", 75, "Sofia", 62];
-const getPeopleWeight = () => { };
+
+const peopleArr = ['Juan', 73, 'Maria', 64, 'Martin', 74, 'Sofia', 55];
+const peopleObject = {};
+
+const getPeopleWeight = (arr) => {
+  for (let i = 0; i < arr.length; i += 2) {
+    const key = arr[i];
+    const value = arr[i + 1];
+    if (value % 2 === 0 && value > 60) {
+      peopleObject[key] = value;
+    }
+  }
+  console.log(peopleObject);
+  return peopleObject;
+};
+
+getPeopleWeight(peopleArr);
 
 // ///////////////////////////////////////////////////////////////
 // ⬇️ NO TOCAR ESTA PARTE O ROMPERÁS LOS TESTS ⬇️
