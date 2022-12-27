4a2ed9f775c27cece80afbfe942270f13d680f63
pablocordonie <pablogarcia.93.06@gmail.com>
2022-12-27 22:56:31 +0100

    JS-Functions Exercise -> solve the exercise 4 & 5 from this file

--- Modulo-1/3-javascript/2-ejercicios/extra-js/js-functions/app.js
+++ Modulo-1/3-javascript/2-ejercicios/extra-js/js-functions/app.js
@@ -65,3 +65,53 @@ function average(arr) {
 }
 
 average(moreNumbers);
+
+// Iteración #5: Calcular promedio de strings
+
+/* Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. */
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'Rock', 8, 'Code'];
+
+function averageWord(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+        if (typeof arr[i] === 'number') {
+            sum = sum + arr[i];
+        } else {
+            sum = sum + arr[i].length
+        }
+    }
+    return sum / arr.length;
+}
+
+averageWord(mixedElements);
+
+// Iteración #6: Valores únicos
+
+/* Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. */
+
+const foods = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+];
+
+function removeDuplicates(arr) {
+    let uniqueFoods = [];
+    for (let i = 0; i < arr.length; i += 1) {
+        if (!uniqueFoods.includes(arr[i])) {
+            uniqueFoods.push(arr[i]);
+        }
+    }
+    return uniqueFoods;
+}
+
+removeDuplicates(foods);
