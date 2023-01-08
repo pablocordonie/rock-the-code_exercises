2115834136b78051b3d5a30d221d38ab56911e48
pablocordonie <pablogarcia.93.06@gmail.com>
2023-01-09 00:41:19 +0100

    Review-DOM Exercise -> solve the first 3 requirements for this exercise

--- Modulo-1/3-javascript/2-ejercicios/extra-js/review-DOM/app.js
+++ Modulo-1/3-javascript/2-ejercicios/extra-js/review-DOM/app.js
@@ -2,3 +2,39 @@
 dinámicamente en el html que imprima cada uno de los paises. */
 
 const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
+
+const createLiElementsInsideANewUl = (arr) => {
+    const ulElement = document.createElement('ul');
+    for (country of arr) {
+        const liElement = document.createElement('li');
+        liElement.innerText = country;
+        ulElement.appendChild(liElement);
+    }
+    return ulElement;
+}
+
+document.body.appendChild(createLiElementsInsideANewUl(countries));
+
+// 1.2 Elimina el elemento que tenga la clase .fn-remove-me.
+
+const pToRemove = document.querySelector('.fn-remove-me');
+
+pToRemove.remove();
+
+/* 1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos 
+en el div de html con el atributo data-function="printHere". */
+
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const createLiElementsInsideAnExistingDiv = (arr) => {
+    const ulElement = document.createElement('ul');
+    for (car of arr) {
+        const liElement = document.createElement('li');
+        liElement.innerText = car;
+        ulElement.appendChild(liElement);
+    }
+    return ulElement;
+}
+
+const dataFunctionDiv = document.querySelector('[data-function="printHere"]');
+dataFunctionDiv.appendChild(createLiElementsInsideAnExistingDiv(cars));
